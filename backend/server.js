3a4cd040b3e734ac07a8cc0a56a552b97f8f60b9
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-07 22:10:49 +0530

    Completed the setup route of user having name, email, and password, tested the route and database has the data stored in it, Now working on bcryptjs

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,46 @@ async function main() {
     await mongoose.connect(mongoURI);
 }
 
+// Create Route
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required!",
+            });
+        }
+
+        const existingUser= await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+        
+        const newUser = new User({
+            name,
+            email,
+            password,
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({
+            message: "User Registered Successfully",
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
+
+});
+
 app.listen(port, () => {
     console.log(`App listening on the given port: ${port}`);
 });
