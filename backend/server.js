41ba0d99dcb92246e4a439bcb65a2d08b6d460cc
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-08 10:22:40 +0530

    Implemented login API with JWT Auhentication

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
-const port = process.env.PORT || 5000;
-const mongoURI = process.env.MONGO_URI;
-// const secret = process.env.JWT_SECRET;
 const User = require("./models/User.js");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const port = process.env.PORT || 5000;
+const mongoURI = process.env.MONGO_URI;
+const secret = process.env.JWT_SECRET;
 
 app.use(express.json());
 app.use(cors());
@@ -35,8 +37,9 @@ app.post("/register", async (req, res) => {
             });
         }
 
-        const existingUser= await User.findOne({email});
-        if(existingUser){
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
             return res.status(400).json({
                 message: "User already exists",
             });
@@ -63,6 +66,45 @@ app.post("/register", async (req, res) => {
             message: "Internal Server Error",
         });
     }
+
+});
+
+app.post("/login", async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "All fields are required!",
+        });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid Credentials",
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch === false) {
+        return res.status(400).json({
+            message:"Password is incorrect",
+        });
+    }
+
+    const token=jwt.sign(
+        {userId:user._id},
+        secret,
+        {expiresIn:"1h"},
+    );
+
+    return res.status(200).json({
+        message:"Login Successful",
+        token,
+    });
 
 });
 
