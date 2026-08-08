8cac96acb37e60089bc2c17f33fd37f1220551cf
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-08-08 15:31:01 +0530

    Completed the Protected Route part

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-        <Route path="/profile" element={<h1>Profile</h1>} />
-        <Route path="/change-password" element={<h1>Change Password</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                {/* Public Routes */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                {/* Protected Routes */}
+                <Route element={<ProtectedRoute />}>
+
+                    <Route
+                        path="/dashboard"
+                        element={<h1>Dashboard</h1>}
+                    />
+
+                    <Route
+                        path="/profile"
+                        element={<h1>Profile</h1>}
+                    />
+
+                    <Route
+                        path="/change-password"
+                        element={<h1>Change Password</h1>}
+                    />
+
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
